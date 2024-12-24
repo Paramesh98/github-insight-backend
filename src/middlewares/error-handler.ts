e417c5931d88f732ee46a6ea2ae0b7c82d187b7b
Paramesh Krishna <parameshkrishna98@gmail.com>
2024-12-24 18:40:24 +0100

    feat: add logger for error handler

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
+import { logger } from '../config/logger.js';
 import { StatusCodes } from '../enums/status-codes.js';
 
 export const errorHandler = (err: Error, _req: Request, res: Response) => {
   const status = StatusCodes.INTERNAL_SERVER_ERROR;
+  logger.error(err.message);
   res.status(status).json({
     error: {
       message: err.message || 'Internal Server Error',

35e911a56e87ac0b76d61d85fe2b20cb8295e981
Paramesh Krishna <parameshkrishna98@gmail.com>
2024-12-26 17:08:58 +0100

    feat: initialize git with jest

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,18 @@
 import cors from 'cors';
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { config } from './config/env.js';
 import { logger } from './config/logger.js';
-import { StatusCodes } from './enums/status-codes.js';
 import { errorHandler } from './middlewares/error-handler.js';
 import { notFound } from './middlewares/not-found.js';
 import routes from './routes/user.routes.js';
 
-const app = express();
+export const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/api', routes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = 8081;
-
-app.get('/', (req: Request, res: Response) => {
-  const successStatus = StatusCodes.OK;
-  res.status(successStatus).send('Hello World');
-});
-
-app.listen(PORT, () => {
-  logger.info(`Server listening at port ${PORT}`);
+export const server = app.listen(config.PORT, () => {
+  logger.info(`Server listening at port ${config.PORT}`);
 });

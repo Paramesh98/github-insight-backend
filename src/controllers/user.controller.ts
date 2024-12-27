aa42a58f28bdf5e2edb5cb365e9f0857daf69e44
Paramesh Krishna <parameshkrishna98@gmail.com>
2024-12-27 13:43:59 +0100

    fix: clean up

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { logger } from '../config/logger.js';
 import { StatusCodes } from '../enums/status-codes.js';
 import * as githubService from '../services/github.service.js';
 
@@ -10,6 +11,7 @@ export const getUser = async (req: Request, res: Response) => {
     const user = await githubService.fetchUser(username, page as string, per_page as string);
     res.status(StatusCodes.OK).json({ success: true, data: user });
   } catch (err) {
+    logger.error((err as Error)?.message);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       error: {
         message: (err as Error).message || 'Internal Server Error',
@@ -26,6 +28,7 @@ export const getRepos = async (req: Request, res: Response) => {
     const repos = await githubService.fetchRepos(username, page as string, per_page as string);
     res.status(StatusCodes.OK).json({ success: true, data: repos });
   } catch (err) {
+    logger.error((err as Error)?.message);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       error: {
         message: (err as Error).message || 'Internal Server Error',
@@ -42,6 +45,7 @@ export const getGists = async (req: Request, res: Response) => {
     const gists = await githubService.fetchGists(username, page as string, per_page as string);
     res.status(StatusCodes.OK).json({ success: true, data: gists });
   } catch (err) {
+    logger.error((err as Error)?.message);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       error: {
         message: (err as Error).message || 'Internal Server Error',
@@ -58,6 +62,7 @@ export const getOrgs = async (req: Request, res: Response) => {
     const orgs = await githubService.fetchOrgs(username, page as string, per_page as string);
     res.status(StatusCodes.OK).json({ success: true, data: orgs });
   } catch (err) {
+    logger.error((err as Error)?.message);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       error: {
         message: (err as Error).message || 'Internal Server Error',
